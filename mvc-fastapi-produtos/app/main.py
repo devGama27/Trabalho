8d28672c7808b80adddfe1035a8b73dc9b3247de
devGama27 <devcaiogama@gmail.com>
2025-09-29 14:58:10 -0300

    Add files via upload

--- mvc-fastapi-produtos/app/main.py
+++ mvc-fastapi-produtos/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from app.routers.product_router import router
 
-app = FastAPI()
+app = FastAPI(
+    title="API de Produtos",
+    description="API simples para CRUD de produtos usando FastAPI, SQLAlchemy e SQLite",
+    version="1.0.0"
+)
+
+app.include_router(router)
 
 @app.get("/")
 def read_root():
     return {"message": "API funcionando!"}
-
